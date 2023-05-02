c975786a82cfaa6bf70e47e98f097a2da9593fe9
Bonga-X01 <bbonginkosi18@gmail.com>
2023-05-02 23:04:49 +0200

    expressed assumption in the main class

--- src/main/java/org/nkosiDev/Main.java
+++ src/main/java/org/nkosiDev/Main.java
@@ -2,11 +2,18 @@ package org.nkosiDev;
 
 import java.util.Collection;
 
+/**
+ * Assumptions:
+ * Input is a string of unique integers in ascending order.
+ *
+ */
+
 public class Main {
     public static void main(String[] args) {
 
         NumberRangeSummarizer numberRangeSummarizerImpl = new NumberRangeSummarizerImpl();
-        Collection<Integer> integerCollection = numberRangeSummarizerImpl.collect("");
+        String input = "1,2,4,5,6,8,10,11,12";
+        Collection<Integer> integerCollection = numberRangeSummarizerImpl.collect(input);
         String summaryString = numberRangeSummarizerImpl.summarizeCollection(integerCollection);
         System.out.println(summaryString);
     }
